6cc9151f9e1f36b9c0c88808dd9093cc7189d42a
Leandro <ferreir720@gmail.com>
2026-09-16 18:11:38 -0300

    fix: corrrigir imagem do icone de question boxes

--- script/script.js
+++ script/script.js
@@ -1,104 +1,25 @@
-const questions = document.querySelector(".questions"); //  Seleciona o pai principal.
+// O QUE?
+// 1 - Botão de "+ " se torna "-".
+// 2 - O texto aparece em baixo do título das perguntas. // Concluído!
 
-const imagem1 = questions.querySelector(".image-1");
-const imagem2 = questions.querySelector(".image-2"); // ---------------------------------------
-const imagem3 = questions.querySelector(".image-3"); //  Seleciona cada imagem de question-box.
-const imagem4 = questions.querySelector(".image-4"); // ---------------------------------------
+// COMO?
+// 1 - Substituir a imagem do botão "+" para uma outro imagem de "-".
+// 2 - Usar o "display: block" no texto.    //  concluído!
 
-// -----------------------------------------------------------------------------------------
+// QUANDO?
+// 1 - O usuário clicar ou no título do texto ou na imagem de "+".
+// ---------------------------------------------------------------------------------
 
-const titleImage1 = questions.querySelector(".title-image1"); //  Selecoina o título 1.
-console.log(titleImage1);
+const questionBoxes = document.getElementsByClassName("question-boxes");
+const title = document.getElementsByClassName("title");
 
-const p1 = questions.querySelector(".p1"); //  Seleciona o parágrafo 1.
-console.log(p1);
-
-titleImage1.addEventListener("click", () => {
-  console.log(p1);
-  p1.style.display = "block";
-  p2.style.display = "none";
-  p3.style.display = "none";
-  p4.style.display = "none";
-});
-
-imagem1.addEventListener("click", () => {
-  console.log(p1);
-  p1.style.display = "block";
-  p2.style.display = "none";
-  p3.style.display = "none";
-  p4.style.display = "none";
-});
-
-// -----------------------------------------------------------------------------------------
-
-const titleImage2 = questions.querySelector(".title-image2"); //  Selecoina o título 2.
-console.log(titleImage2);
-
-const p2 = questions.querySelector(".p2"); //  Seleciona o parágrafo 2.
-console.log(p2);
-
-titleImage2.addEventListener("click", () => {
-  console.log(p2);
-  p1.style.display = "none";
-  p2.style.display = "block";
-  p3.style.display = "none";
-  p4.style.display = "none";
-});
-
-imagem2.addEventListener("click", () => {
-  console.log(p1);
-  p1.style.display = "block";
-  p2.style.display = "none";
-  p3.style.display = "none";
-  p4.style.display = "none";
-});
-
-// -----------------------------------------------------------------------------------------
-
-const titleImage3 = questions.querySelector(".title-image3"); //  Selecoina o título 3.
-console.log(titleImage3);
-
-const p3 = questions.querySelector(".p3"); //  Seleciona o parágrafo 3.
-console.log(p3);
-
-titleImage3.addEventListener("click", () => {
-  console.log(p3);
-  p1.style.display = "none";
-  p2.style.display = "none";
-  p3.style.display = "block";
-  p4.style.display = "none";
-});
-
-imagem3.addEventListener("click", () => {
-  console.log(p1);
-  p1.style.display = "block";
-  p2.style.display = "none";
-  p3.style.display = "none";
-  p4.style.display = "none";
-});
-
-// -----------------------------------------------------------------------------------------
-
-const titleImage4 = questions.querySelector(".title-image4"); //  Selecoina o título 3.
-console.log(titleImage4);
-
-const p4 = questions.querySelector(".p4"); //  Seleciona o parágrafo 3.
-console.log(p4);
-
-titleImage4.addEventListener("click", () => {
-  console.log(p4);
-  p1.style.display = "none";
-  p2.style.display = "none";
-  p3.style.display = "none";
-  p4.style.display = "block";
-});
-
-imagem4.addEventListener("click", () => {
-  console.log(p1);
-  p1.style.display = "block";
-  p2.style.display = "none";
-  p3.style.display = "none";
-  p4.style.display = "none";
-});
-
-// ----------------------------------------------------------------------------------------------------------- //
+for (const item of questionBoxes) {
+  item.addEventListener("click", () => {
+    const image = item.getElementsByClassName("image")[0];
+    const text = item.getElementsByClassName("text")[0];
+    const image2 = item.getElementsByClassName("image-icon")[0];
+    text.classList.toggle("hidden");
+    image.classList.toggle("image-icon");
+    image2.classList.toggle("hidden");
+  });
+}
